753cb393d78b1bd7e96261d8cca4d3c5387011a4
dreamboxtech <bbbolaleye@gmail.com>
2022-12-01 03:16:07 +0100

    end points modified with pu and post requests

--- drinks/views.py
+++ drinks/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import request,render,HttpResponse
+from django.shortcuts import render,HttpResponse
 from django.http import JsonResponse
 from .serializers import DrinkSerializer
 from .models import Drink
+from rest_framework.response import Response
+from rest_framework import status
 
+from rest_framework.decorators import api_view
 
+@api_view(['GET', 'POST'])
 def drink_list(request):
-	drinks =  Drink.objects.all()
-	return render(request, template_name)
+
+	if request.method == "GET":
+		drinks =  Drink.objects.all()
+		serializer  = DrinkSerializer(drinks, many=True)
+		return JsonResponse({"drinks": serializer.data}, safe=False)
+
+	if request.method == "POST":
+		serializer = DrinkSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+
+
+
